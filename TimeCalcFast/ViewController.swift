3ed6bc9c504e2568d6ba0bd01143f101fd077161
Artem <klufone@mail.ru>
2016-11-06 15:12:20 +0300

    Initial Commit

---
+++ TimeCalcFast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeCalcFast
+//
+//  Created by артем on 06.11.16.
+//  Copyright © 2016 артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
